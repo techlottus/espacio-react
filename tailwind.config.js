3345933df71e430a102b9c15290d46a46063113a
alan llamas <alanllamasg@gmail.com>
2023-06-29 15:29:28 -0500

    chore: modified tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ import * as tailwindBase from "@lottuseducation/tailwind-base";
 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,*.js}",
-    "./node_modules/@lottuseducation/design_system/**/*.js"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@lottuseducation/design_system/**/*.{js,jsx,ts,tsx}"
   ],
   presets: [tailwindBase.default],
   theme: {
