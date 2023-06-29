62012860921fe2e4f601d2f8b5c8edbb1148b820
alan llamas <alanllamasg@gmail.com>
2023-06-29 17:54:06 -0500

    chore: added fonts correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,18 @@ import * as tailwindBase from "@lottuseducation/tailwind-base";
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/@lottuseducation/design_system/**/*.{js,jsx,ts,tsx}"
+    "./node_modules/@lottuseducation/design_system/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [tailwindBase.default],
   theme: {
     extend: {
+      fontFamily: {
+        principal: ["Nunito"],
+        secondary: ["Poppins"],
+        icons: ["Material Icons"],
+        "icons-outlined": ["Material Icons Outlined"],
+        "icons-solid": ["Material Icons"],
+      },
       colors:{
         primary: {
           500: "#e04504",
